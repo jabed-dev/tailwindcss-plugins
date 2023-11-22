c40d20773b0b97ebdbbdb7af210ec52b0af9b175
Jabed Hossain <59810760+jabed-dev@users.noreply.github.com>
2023-11-22 14:18:41 +0600

    Create e.js

---
+++ plugins/e.js
@@ -0,0 +1,26 @@
+const plugin = require('tailwindcss/plugin')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    plugin(({ addUtilities, e }) => {
+      // console.log(e(`~\`!@#$%^&*()=+[]{}\\|;:'",./<>?`))
+      // \~\`\!\@\#\$\%\^\&\*\(\)\=\+\[\]\{\}\\\|\;\:\'\"\2c \.\/\<\>\?
+
+      addUtilities({
+        [`.${e('w/50%')}`]: { // e function escaping strings
+          width: 'calc(100% / 2)',
+          backgroundColor: 'red',
+        },
+        [`.x\\/50\\%`]: { // manually escaping strings
+          width: 'calc(100% / 2)',
+          backgroundColor: 'green',
+        },
+      })
+    }),
+  ],
+}
