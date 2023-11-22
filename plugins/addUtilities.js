0059a9f86e89029d811e7c00c9abf53dbf22846d
Jabed Hossain <59810760+jabed-dev@users.noreply.github.com>
2023-11-22 22:57:48 +0600

    Update addUtilities.js

--- plugins/addUtilities.js
+++ plugins/addUtilities.js
@@ -1,15 +1,32 @@
 const plugin = require('tailwindcss/plugin')
 
-module.exports = plugin(({ addUtilities }) => {
-      addUtilities({
-        '.content-auto': {
-          'content-visibility': 'auto',
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  prefix: 'tw-',
+  important: true,
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    plugin(({ addUtilities }) => {
+      addUtilities(
+        {
+          '.content-auto': {
+            'content-visibility': 'auto',
+          },
+          '.content-hidden': {
+            'content-visibility': 'hidden',
+          },
+          '.content-visible': {
+            'content-visibility': 'visible',
+          },
         },
-        '.content-hidden': {
-          'content-visibility': 'hidden',
-        },
-        '.content-visible': {
-          'content-visibility': 'visible',
-        },
-      })
-    })
+        {
+          respectPrefix: false, // true and prefix: 'wt-' then add 'wt-*'
+          respectImportant: false, // true and important: true then add '* !important'
+        }
+      )
+    }),
+  ],
+}
