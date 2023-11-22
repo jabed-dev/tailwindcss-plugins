e5f385e0ba082e2a6203610cc9efaa15d28e15c0
Jabed Hossain <59810760+jabed-dev@users.noreply.github.com>
2023-11-22 20:40:33 +0600

    Update config.js

--- plugins/config.js
+++ plugins/config.js
@@ -2,18 +2,21 @@ const plugin = require('tailwindcss/plugin')
 
 module.exports = {
   content: ['./index.html'],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
   plugins: [
-    plugin(function ({ addBase, config }) {
+    plugin(({ config }) => {
       // config values in the user’s and default Tailwind configuration 
       console.log(config().theme.accentColor.red)
       console.log(config('theme.accentColor.red'))
       console.log(config('content').files)
+      console.log(config('darkMode'))
+      
       // if config('incorrect') undefined then return second argument
-      console.log(config('theme.incorrect', {x: 'abc'}))
-      // return > {x: 'abc'}
+      console.log(config('theme.incorrect', {x: 'xyz'}))
+      // return > {x: 'xyz'}
     }),
   ],
 }
